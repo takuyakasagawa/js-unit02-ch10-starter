9407b2ea88effaacbd9b0200e84241c06b651dd9
Atsuhiro Teshima <hiro@lingualbox.com>
2018-07-24 14:03:22 +0800

    Remove a test about strictly checking the format of password

--- src/lib/PasswordValidator.test.js
+++ src/lib/PasswordValidator.test.js
@@ -31,19 +31,4 @@ describe('password validator', () => {
                 expect(res.message).toBe('パスワードが短すぎます。')
             });
     })
-
-    test("大文字、小文字、数字を全て含まない場合はエラーを返す。", async () => {
-        const password1 = "testPass";
-        const password2 = "testpass1";
-        const password3 = "TESTPASS1";
-        const validator1 = new PasswordValidator(password1);
-        const validator2 = new PasswordValidator(password2);
-        const validator3 = new PasswordValidator(password3);
-        const res1 = await validator1.validate().then(res => res);
-        const res2 = await validator2.validate().then(res => res);
-        const res3 = await validator3.validate().then(res => res);
-        expect(res1.success).toBeFalsy();
-        expect(res2.success).toBeFalsy();
-        expect(res3.success).toBeFalsy();
-    })
 });
