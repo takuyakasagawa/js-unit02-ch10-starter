98168ef247018ad3b4cb2398812826e301b10f3e
takuyakasagawa <kasa.kasa.0062362@gmail.com>
2021-01-04 21:12:48 +0900

    edited js

--- src/lib/MailValidator.js
+++ src/lib/MailValidator.js
@@ -9,10 +9,10 @@ export default class extends BaseValidator {
     return super._cannotEmpty()
       .then(this._checkFormat)
       .then((res) => {
-        return { success: true }
+        return { success: true };
       })
       .catch(err => {
-        return err
+        return err;
       });
   }
   _checkFormat() {
